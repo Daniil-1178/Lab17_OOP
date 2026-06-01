02026b87b1cf74a592af9fd4400769146e184def
Daniil-1178 <daniilzubkov278@gmail.com>
2026-06-01 09:31:16 +0300

    Add files via upload

---
+++ Lab17/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Lab17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtServerLog = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 449);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtServerLog);
+            this.tabPage1.Controls.Add(this.btnStartServer);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 423);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtServerLog
+            // 
+            this.txtServerLog.Location = new System.Drawing.Point(6, 35);
+            this.txtServerLog.Multiline = true;
+            this.txtServerLog.Name = "txtServerLog";
+            this.txtServerLog.ReadOnly = true;
+            this.txtServerLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtServerLog.Size = new System.Drawing.Size(286, 310);
+            this.txtServerLog.TabIndex = 1;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(80, 6);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(115, 23);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "Запустити сервер";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.sendButton);
+            this.tabPage2.Controls.Add(this.messageTextBox);
+            this.tabPage2.Controls.Add(this.chatTextBox);
+            this.tabPage2.Controls.Add(this.logoutButton);
+            this.tabPage2.Controls.Add(this.loginButton);
+            this.tabPage2.Controls.Add(this.userNameTextBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 423);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(7, 140);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "Надіслати";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(7, 101);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(163, 20);
+            this.messageTextBox.TabIndex = 4;
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(212, 15);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.ReadOnly = true;
+            this.chatTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatTextBox.Size = new System.Drawing.Size(247, 330);
+            this.chatTextBox.TabIndex = 3;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(95, 52);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.TabIndex = 2;
+            this.logoutButton.Text = "Вихід";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(7, 52);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Вхід";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(7, 15);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(163, 20);
+            this.userNameTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtServerLog;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.TextBox chatTextBox;
+    }
+}
+
---
+++ Lab17/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    public partial class Form1 : Form
+    {
+        private static TcpListener tcpListener;
+        private List<ServerClientObject> serverClients = new List<ServerClientObject>();
+        private bool isServerRunning = false;
+
+        private TcpClient client;
+        private NetworkStream stream;
+        private bool isClientConnected = false;
+        private string userName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+            sendButton.Enabled = false;
+        }
+
+        protected internal void AppendServerLog(string message)
+        {
+            if (txtServerLog.InvokeRequired)
+            {
+                txtServerLog.Invoke(new Action<string>(AppendServerLog), message);
+                return;
+            }
+            txtServerLog.AppendText($"[{DateTime.Now.ToShortTimeString()}] {message}\r\n");
+        }
+
+        protected internal void AppendChat(string message)
+        {
+            if (chatTextBox.InvokeRequired)
+            {
+                chatTextBox.Invoke(new Action<string>(AppendChat), message);
+                return;
+            }
+            chatTextBox.AppendText(message + "\r\n");
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            if (isServerRunning) return;
+
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                isServerRunning = true;
+                btnStartServer.Enabled = false;
+                AppendServerLog("Сервер запущено. Очікування підключень...");
+
+                Task.Run(() => ListenForClients());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка запуску сервера: " + ex.Message);
+            }
+        }
+
+        private void ListenForClients()
+        {
+            try
+            {
+                while (isServerRunning)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    ServerClientObject clientObject = new ServerClientObject(tcpClient, this);
+                    Task.Run(() => clientObject.Process());
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected internal void AddConnection(ServerClientObject clientObject)
+        {
+            serverClients.Add(clientObject);
+        }
+
+        protected internal void RemoveConnection(string id)
+        {
+            ServerClientObject client = serverClients.FirstOrDefault(c => c.Id == id);
+            if (client != null) serverClients.Remove(client);
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < serverClients.Count; i++)
+            {
+                if (serverClients[i].Id != id)
+                {
+                    try
+                    {
+                        serverClients[i].Stream.Write(data, 0, data.Length);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private void DisconnectServer()
+        {
+            isServerRunning = false;
+            if (tcpListener != null) tcpListener.Stop();
+
+            for (int i = 0; i < serverClients.Count; i++)
+            {
+                serverClients[i].Close();
+            }
+            serverClients.Clear();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userNameTextBox.Text))
+            {
+                MessageBox.Show("Введіть своє ім'я!");
+                return;
+            }
+
+            userName = userNameTextBox.Text.Trim();
+            client = new TcpClient();
+
+            try
+            {
+                client.Connect("127.0.0.1", 8888);
+                stream = client.GetStream();
+
+                byte[] data = Encoding.Unicode.GetBytes(userName);
+                stream.Write(data, 0, data.Length);
+
+                isClientConnected = true;
+                loginButton.Enabled = false;
+                logoutButton.Enabled = true;
+                sendButton.Enabled = true;
+                userNameTextBox.ReadOnly = true;
+
+                AppendChat($"Ласкаво просимо, {userName}!");
+
+                Task.Run(() => ReceiveMessages());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка підключення до сервера: " + ex.Message);
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            try
+            {
+                while (isClientConnected)
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        if (bytes == 0)
+                        {
+                            DisconnectClient();
+                            return;
+                        }
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    AppendChat(message);
+                }
+            }
+            catch
+            {
+                if (isClientConnected)
+                {
+                    AppendChat("Підключення перервано!");
+                    DisconnectClient();
+                }
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(messageTextBox.Text) || !isClientConnected) return;
+
+            try
+            {
+                string message = messageTextBox.Text;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                AppendChat($"{userName}: {message}");
+                messageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка відправки повідомлення: " + ex.Message);
+            }
+        }
+
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            DisconnectClient();
+        }
+
+        private void DisconnectClient()
+        {
+            if (!isClientConnected) return;
+
+            isClientConnected = false;
+            if (stream != null) stream.Close();
+            if (client != null) client.Close();
+
+            if (this.IsHandleCreated)
+            {
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    loginButton.Enabled = true;
+                    logoutButton.Enabled = false;
+                    sendButton.Enabled = false;
+                    userNameTextBox.ReadOnly = false;
+                }));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisconnectClient();
+            DisconnectServer();
+        }
+    }
+
+    public class ServerClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        private string userName;
+        private TcpClient client;
+        private Form1 serverForm;
+
+        public ServerClientObject(TcpClient tcpClient, Form1 form)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            serverForm = form;
+            serverForm.AddConnection(this);
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                userName = GetMessage();
+                if (string.IsNullOrEmpty(userName)) return;
+
+                string message = userName + " увійшов в чат";
+                serverForm.BroadcastMessage(message, this.Id);
+                serverForm.AppendServerLog(message);
+
+                while (true)
+                {
+                    message = GetMessage();
+                    if (string.IsNullOrEmpty(message))
+                    {
+                        message = userName + " покинув чат";
+                        serverForm.AppendServerLog(message);
+                        serverForm.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+
+                    message = string.Format("{0}: {1}", userName, message);
+                    serverForm.AppendServerLog(message);
+                    serverForm.BroadcastMessage(message, this.Id);
+                }
+            }
+            catch
+            {
+                string message = userName + " покинув чат";
+                serverForm.AppendServerLog(message);
+                serverForm.BroadcastMessage(message, this.Id);
+            }
+            finally
+            {
+                serverForm.RemoveConnection(this.Id);
+                Close();
+            }
+        }
+
+        private string GetMessage()
+        {
+            try
+            {
+                byte[] data = new byte[64];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = Stream.Read(data, 0, data.Length);
+                    if (bytes == 0) return null;
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (Stream.DataAvailable);
+
+                return builder.ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        protected internal void Close()
+        {
+            if (Stream != null) Stream.Close();
+            if (client != null) client.Close();
+        }
+    }
+}
